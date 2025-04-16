f6f7bbe83672327af364a2b123f6dc6da0f60df9
KKaChi <rnagpie@gmail.com>
2025-04-16 16:57:03 +0900

    조건문 (if문)

---
+++ src/cond/Conditional02.java
@@ -0,0 +1,59 @@
+package cond;
+
+public class Conditional02 {
+
+	public static void main(String[] args) {
+		
+		
+		int dice =5 ;
+		
+		if (dice == 1 ) { // 5 == 1 -> false
+			System.out.println("한칸 전진");
+		}
+		
+		if (dice == 1) { // 5 == 1 -> false
+			System.out.println("한칸 전진");
+			//
+			//
+		} else {
+			System.out.println("1이 아닐때 할 행동1");
+			System.out.println("1이 아닐때 할 행동2");
+			System.out.println("1이 아닐때 할 행동3");
+		}
+		
+		if (dice == 1)
+			System.out.println("한칸 전진");
+		else
+			System.out.println("1이 아닐때 할 행동 1");
+		
+		
+		System.out.println("1이 아닐때 할 행동 1");
+		System.out.println("1이 아닐때 할 행동 1");
+	/*
+	 if()
+	 if()
+	 if()
+
+	 if()
+	 else
+	 
+	 if()
+	 else if()
+	 else if()
+	 else if()
+	
+	 else  X
+	  
+	  
+	 else if()	X
+	  */
+	
+	
+	
+	
+	
+	}
+	
+
+
+}
